3a80c9fd08c3140a7544adaee9df5339d9506bb4
Joakim Soderberg <joakim.soderberg@gmail.com>
2011-03-03 20:54:18 +0000

    - Playing around some with rendering.
    git-svn-id: http://qw-dev.net/svn/bspshow@5 30bca270-f32b-4801-9bde-2d9ae0a63795

--- main.c
+++ main.c
@@ -8,8 +8,11 @@
 
 #include "bsp.h"
 
+#define PI 3.14159
+
 typedef struct texture_s
 {
+	char name[16];
 	int width;
 	int height;
 	float s[3];
@@ -61,6 +64,149 @@ float x, y, z;
 float direction;
 float updown;
 
+int perspective = 0;
+float zoom = 1;
+
+void draw_bounding_box(dmodel_t *m)
+{ 
+	int render_type = GL_LINE;
+	
+	glDisable(GL_CULL_FACE);
+
+	glPushAttrib(GL_POLYGON_BIT);
+	glPolygonMode(GL_FRONT, render_type);
+	glPolygonMode(GL_BACK, render_type);
+
+	glBegin(GL_POLYGON);
+	{
+		// Top.
+		glColor4f(0.0, 1.0, 1.0, 0.25);
+		glVertex3f(m->mins[0], m->mins[1], m->maxs[2]);
+
+		glColor4f(1.0, 0.0, 0.0, 0.5);
+		glVertex3f(m->mins[0], m->maxs[1], m->maxs[2]);
+
+		glColor4f(0.0, 1.0, 0.0, 0.5);
+		glVertex3f(m->maxs[0], m->maxs[1], m->maxs[2]);
+
+		glColor4f(0.0, 0.0, 1.0, 0.5);
+		glVertex3f(m->maxs[0], m->mins[1], m->maxs[2]);
+	}
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	{
+		// Bottom.
+		glColor4f(0.0, 1.0, 1.0, 0.25);
+		glVertex3f(m->mins[0], m->mins[1], m->mins[2]);
+
+		glColor4f(1.0, 0.0, 0.0, 0.5);
+		glVertex3f(m->mins[0], m->maxs[1], m->mins[2]);
+
+		glColor4f(0.0, 1.0, 0.0, 0.5);
+		glVertex3f(m->maxs[0], m->maxs[1], m->mins[2]);
+
+		glColor4f(0.0, 0.0, 1.0, 0.5);
+		glVertex3f(m->maxs[0], m->mins[1], m->mins[2]);
+	}
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	{
+		// Side 1.
+		glColor4f(0.0, 1.0, 1.0, 0.25);
+		glVertex3f(m->mins[0], m->mins[1], m->mins[2]);
+
+		glColor4f(1.0, 0.0, 0.0, 0.5);
+		glVertex3f(m->mins[0], m->mins[1], m->maxs[2]);
+
+		glColor4f(0.0, 1.0, 0.0, 0.5);
+		glVertex3f(m->mins[0], m->maxs[1], m->maxs[2]);
+
+		glColor4f(0.0, 0.0, 1.0, 0.5);
+		glVertex3f(m->mins[0], m->maxs[1], m->mins[2]);
+	}
+	glEnd();	
+
+	glBegin(GL_POLYGON);
+	{
+		// Side 2.
+		glColor4f(0.0, 1.0, 1.0, 0.25);
+		glVertex3f(m->mins[0], m->mins[1], m->mins[2]);
+
+		glColor4f(1.0, 0.0, 0.0, 0.5);
+		glVertex3f(m->mins[0], m->mins[1], m->maxs[2]);
+
+		glColor4f(0.0, 1.0, 0.0, 0.5);
+		glVertex3f(m->maxs[0], m->mins[1], m->maxs[2]);
+
+		glColor4f(0.0, 0.0, 1.0, 0.5);
+		glVertex3f(m->maxs[0], m->mins[1], m->mins[2]);
+	}
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	{
+		// Side 3.
+		glColor4f(0.0, 1.0, 1.0, 0.25);
+		glVertex3f(m->maxs[0], m->mins[1], m->mins[2]);
+
+		glColor4f(1.0, 0.0, 0.0, 0.5);
+		glVertex3f(m->maxs[0], m->mins[1], m->maxs[2]);
+
+		glColor4f(0.0, 1.0, 0.0, 0.5);
+		glVertex3f(m->maxs[0], m->maxs[1], m->maxs[2]);
+
+		glColor4f(0.0, 0.0, 1.0, 0.5);
+		glVertex3f(m->maxs[0], m->maxs[1], m->mins[2]);
+	}
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	{
+		// Side 4.
+		glColor4f(0.0, 1.0, 1.0, 0.25);
+		glVertex3f(m->mins[0], m->maxs[1], m->mins[2]);
+
+		glColor4f(1.0, 0.0, 0.0, 0.5);
+		glVertex3f(m->mins[0], m->maxs[1], m->maxs[2]);
+
+		glColor4f(0.0, 1.0, 0.0, 0.5);
+		glVertex3f(m->maxs[0], m->maxs[1], m->maxs[2]);
+
+		glColor4f(0.0, 0.0, 1.0, 0.5);
+		glVertex3f(m->maxs[0], m->maxs[1], m->mins[2]);
+	}
+	glEnd();
+
+	glEnable(GL_CULL_FACE);
+
+	glPopAttrib();
+}
+
+void draw_text(float x, float y, float z, const char *format, ...)
+{
+	static char buf[512];
+	const char *c;
+	va_list args;
+
+	buf[0] = NULL;
+	
+	glRasterPos3f(x, y,z);
+
+	va_start(args, format);
+	vsnprintf(buf, sizeof(buf), format, args);
+
+	for (c = buf; *c != '\0'; c++) 
+	{
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *c);
+	}
+	
+	va_end(args);
+}
+
+float clip_height = 1.0;
+
 void draw_face(dface_t *face)
 {
 	dedge_t *edge;
@@ -68,18 +214,38 @@ void draw_face(dface_t *face)
 	int e;
 	int lindex;
 	float s, t;
+	float max_z = models[0].maxs[2];
+	float min_z = models[0].mins[2];
 	texture_t *texture = &textures[face->texinfo];
+	float *normal = planes[face->planenum].normal;
 	
+	// Don't draw walls in orthogonal projection.
+	if (!perspective && (normal[2] > -0.5) && (normal[2] < 0.5))
+		return;
+
+	// Enable a clipping plane, so that a percentage of the absolute top of the level isn't drawn.
+	{
+		double plane[4] = {0.0, 0.0, -1.0, 0.0};
+		glPushMatrix();
+   		glEnable(GL_CLIP_PLANE0);
+		glTranslatef(0.0, 0.0, max_z * clip_height);
+   		glClipPlane(GL_CLIP_PLANE0, plane);
+		glPopMatrix();
+	}
+
 	// Set the normal of the face.
-	glNormal3fv(planes[face->planenum].normal);
+	glNormal3fv(normal);
 
 	// Draw the face.
 	glBegin(GL_POLYGON);
 	{
+		if (strstr(texture->name, "sky"))
+			return;
+
 		glEnable(GL_TEXTURE_2D);
 		glBindTexture(GL_TEXTURE_2D, texture->texnum);
 
-		for (e = 0; e < face->numedges; e ++)
+		for (e = 0; e < face->numedges; e++)
 		{
 			lindex = ledges[face->firstedge + e];
 
@@ -94,10 +260,75 @@ void draw_face(dface_t *face)
 				v = &vertices[edge->v[1]];
 			}
 
+			{
+				float c = fabs(v->point[2] / (max_z - min_z));
+				glColor4f(c, c, c, 0.5);
+			}
+			glTexCoord2d(0.0, 0.0);
+			glTexCoord2d(0.0, 1.0);
+			glTexCoord2d(1.0, 0.0);
+			glTexCoord2d(1.0, 1.0);
 			glVertex3fv(v->point);
 		}
 	}
 	glEnd();
+
+	glDisable(GL_CLIP_PLANE0);
+}
+
+void draw_axis()
+{
+	float size = 525.0;
+	//glDepthFunc(GL_ALWAYS);     // to avoid visual artifacts with grid lines
+    //glDisable(GL_LIGHTING);
+
+    // draw axis
+    glLineWidth(3);
+    glBegin(GL_LINES);
+        glColor3f(1, 0, 0);
+        glVertex3f(0, 0, 0);
+        glVertex3f(size, 0, 0);
+        glColor3f(0, 1, 0);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, size, 0);
+        glColor3f(0, 0, 1);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 0, size);
+    glEnd();
+    glLineWidth(1);
+
+    // draw arrows(actually big square dots)
+    glPointSize(5);
+    glBegin(GL_POINTS);
+        glColor3f(1, 0, 0);
+        glVertex3f(size, 0, 0);
+        glColor3f(0, 1, 0);
+        glVertex3f(0, size, 0);
+        glColor3f(0, 0, 1);
+        glVertex3f(0, 0, size);
+    glEnd();
+    glPointSize(1);
+
+    // restore default settings
+    //glEnable(GL_LIGHTING);
+    //glDepthFunc(GL_LEQUAL);
+}
+
+void place_camera()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	if (perspective)
+	{
+		gluPerspective(70.0, 1.0, 1.0, 5000.0);
+		gluLookAt(x, y, z, cx, cy, cz, 0, 0.0, 1.0);
+	}
+	else
+	{
+		glOrtho(0.0, zoom * 800, zoom * 600, 0, -99999, 99999);
+		glTranslatef(x, y, -100.0);
+	}
 }
 
 float angle;
@@ -105,32 +336,35 @@ void renderScene(void)
 {
 	size_t i;
 	size_t j;
+
+	place_camera();
+
 	glMatrixMode(GL_MODELVIEW);
 	
 	glClearDepth(1.0);
 
-	//cx = x + cos(direction) * cos(updown);
-	//cy = y + sin(direction) * cos(updown);
-	//cz = z + sin(updown);
-	cx = -50.0;
-	cy = -50.0;
-	cz = 50.0;
+	cx = 0.0;
+	cy = 0.0;
+	cz = 0.0;
 
-	glClearColor (0.0,0.0,0.0,1.0);
-	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0,0.0,0.0,1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	gluLookAt(x, y, z, cx, cy, cz, 0, 0.0, 1.0);
 
-	//glColor3f(0.0, 1.0, 0.0);
-	
 	for (j = 0; j < model_count; j++)
 	{
 		for (i = models[j].firstface; i < (size_t)models[j].numfaces; i++)
 		{
-			//glColor3f(rand() / (float)RAND_MAX, rand() / (float)RAND_MAX, rand() / (float)RAND_MAX);
 			draw_face(&faces[i]);
 		}
 	}
+
+	glColor3f(1.0, 0.0, 0.0);
+	draw_text(300.0, 300.0, 0.0, "(%f, %f)", x, y);
+
+	draw_axis();
+
+	draw_bounding_box(&models[0]);
 
 	glPopMatrix();
 	glutSwapBuffers();
@@ -201,7 +435,7 @@ int read_textures()
 		{
 			texinf = &textureinfos[texnum];
 
-			t = &textures[texture_count++];
+			t = &textures[texnum];
 			t->texnum = texnum;
 
 			vector_copy(t->s, texinf->vectorS);
@@ -211,6 +445,9 @@ int read_textures()
 
 			// Get the pointer to the mipmap.
 			mip = mipmap_ptrs[texinf->miptex];
+			strcpy(t->name, mip->name);
+			t->width = mip->width;
+			t->height = mip->height;
 
 			// Upload the texture data to OpenGL.
 			for (mip_level = 0; mip_level < MIPLEVELS - 1; mip_level++)
@@ -220,7 +457,8 @@ int read_textures()
 				free(texture_data);
 			}
 
-			textures[texnum].is_loaded = 1;
+			t->is_loaded = 1;
+			texture_count++;
 		}
 	}
 
@@ -257,6 +495,62 @@ void init_lighting()
     glEnable (GL_LIGHT0);
 }
 
+const int window_width = 800;
+const int window_height = 600;
+int left_mouse = 0;
+int right_mouse = 0;
+int down_mouse_x;
+int down_mouse_y;
+
+void on_mouse(int button, int state, int mouse_x, int mouse_y)
+{
+	right_mouse = ((state == GLUT_DOWN) && (button == GLUT_RIGHT_BUTTON));
+	left_mouse = ((state == GLUT_DOWN) && (button == GLUT_LEFT_BUTTON));
+	down_mouse_x = mouse_x;
+	down_mouse_y = mouse_y;
+}
+
+void on_mouse_move(int mouse_x, int mouse_y)
+{
+	if (right_mouse)
+	{
+		x = (float)(x - down_mouse_x);
+		y = (window_height) - (float)(y - down_mouse_y);
+	}
+
+	down_mouse_x = mouse_x;
+	down_mouse_y = mouse_y;
+}
+
+void on_keyboard(unsigned char key, int key_x, int key_y)
+{
+	if (key == 'p')
+	{
+		perspective = !perspective;
+	}
+
+	if (key == '+')
+	{
+		clip_height += 0.01f;
+	}
+
+	if (key == '-')
+		clip_height -= 0.01f;
+}
+
+void on_special_key(int key, int key_x, int key_y)
+{
+	switch (key)
+	{
+		case GLUT_KEY_LEFT:		x += 10.0f; break;
+		case GLUT_KEY_RIGHT:	x += 10.0f; break;
+		case GLUT_KEY_UP:		y += 10.0f; break;
+		case GLUT_KEY_DOWN:		y -= 10.0f; break;
+		case GLUT_KEY_PAGE_UP:	zoom += 0.1f; break;
+		case GLUT_KEY_PAGE_DOWN: zoom -= 0.1f; break;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (argc < 2)
@@ -269,11 +563,15 @@ int main(int argc, char **argv)
 		return -1;
 
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(800,600);
+	//glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(window_width, window_height);
 	glutCreateWindow("SnowMen");
 	glutDisplayFunc(renderScene);
 	glutIdleFunc(renderScene);
+	//glutMouseFunc(on_mouse);
+	//glutMotionFunc(on_mouse_move);
+	glutKeyboardFunc(on_keyboard);
+	glutSpecialFunc(on_special_key);
 	
 	srand(time(NULL));
 
@@ -282,17 +580,23 @@ int main(int argc, char **argv)
 	z = 2000;
 	direction = 1.745f;
 	updown = 0.61f;
-	
+		
 	glEnable(GL_TEXTURE_2D);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(60.0, 1.0, 1.0, 5000.0);
+	//gluPerspective(70.0, 1.0, 1.0, 5000.0);
 
 	glEnable(GL_CULL_FACE);
-	glCullFace(GL_FRONT);
+	glCullFace(GL_BACK);
 
-	init_lighting();
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	
+	//init_lighting();
+
+	glMatrixMode(GL_MODELVIEW);
+	glRotatef(180.0, 0.0, 0.0, 1.0);
 	
 	glutMainLoop();
 	
