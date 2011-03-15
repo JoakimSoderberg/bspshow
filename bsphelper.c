a8f49a42e02181ff7bf6052455535c47ff8f969d
Joakim Soderberg <joakim.soderberg@gmail.com>
2011-03-15 22:38:40 +0000

    - Started doing some helper functions, making it easier to use the faces. polygon_t will hold all the information you need to render a textured face.
    git-svn-id: http://qw-dev.net/svn/bspshow@10 30bca270-f32b-4801-9bde-2d9ae0a63795

--- bsphelper.c
+++ bsphelper.c
@@ -56,71 +56,92 @@ float calculate_face_area(dface_t *face)
 }
 
 
-void get_polygons()
+polygon_t *get_polygons(size_t *count)
 {
-	/*
-	for (e = 0; e < face->numedges; e++)
-	{
-		lindex = ledges[face->firstedge + e];
+	size_t i;
+	size_t e;
+	int lindex;
+	dface_t *face;
+	dvertex_t *vertices;
+	dedge_t *edge;
+	dvertex_t *v;
 
-		if (lindex > 0)
-		{					
-			edge = &edges[lindex];
-			v = &vertices[edge->v[0]];
-		}
-		else
+	// TODO: Make sure everything we need is already loaded.
+	polygon_t *polygons = (polygon_t *)calloc(face_count, sizeof(polygon_t));
+
+	for (i = 0; i < face_count; i++)
+	{
+		// Save the face.
+		face = &faces[i];
+		polygons[i].face = face;
+
+		// Save the plane the face is in.
+		polygons[i].plane = &planes[face->planenum];
+
+		// Allocate memory for the edges and vertices.
+		polygons[i].edges = (dedge_t **)calloc(face->numedges, sizeof(dedge_t *));
+		polygons[i].vertices = (dvertex_t **)calloc(face->numedges, sizeof(dvertex_t));
+
+		// Get the edges of the face.
+		for (e = 0; e < face->numedges; e++)
 		{
-			edge = &edges[-lindex];
-			v = &vertices[edge->v[1]];
+			// The index in the ledge list can also be negative,
+			// the sign is just an indication what way we should walk
+			// around the edge of the face. We should always use the absolute
+			// value as the actual index.
+			lindex = ledges[face->firstedge + e];
+
+			if (lindex > 0)
+			{					
+				edge = &edges[lindex];
+				v = &vertices[edge->v[0]];
+			}
+			else
+			{
+				edge = &edges[-lindex];
+				v = &vertices[edge->v[1]];
+			}
+
+			// Add the vertex to the list of face vertices.
+			polygons[i].vertices[e] = v;
+
+			// Save the edge.
+			polygons[i].edges[e] = edge;
 		}
-
-		vertex_color = fabs(v->point[2] / (max_z - min_z));
-		glColor4f(vertex_color, vertex_color, vertex_color, 0.5);
-
-		glTexCoord2d(0.0, 0.0);
-		glTexCoord2d(0.0, 1.0);
-		glTexCoord2d(1.0, 0.0);
-		glTexCoord2d(1.0, 1.0);
-		glVertex3fv(v->point);
-	}
-	*/
-}
-
-unsigned char *expand_texture(const miptex_t *mip, const byte *texture, int mip_level)
-{
-	size_t i;
-	size_t width = mip->width / (int)pow(2.0, mip_level);
-	size_t height = mip->height / (int)pow(2.0, mip_level);
-	size_t expanded_size = (width * height * 3);
-	byte *expanded_data = (byte *)malloc(expanded_size);
-
-	// Convert the image from indexed palette values to RGB values for each pixel.
-	for (i = 0; i < expanded_size; i++)
-	{
-		memcpy(&expanded_data[i*3], &quake_pallete[texture[i]], 3);
 	}
 }
 
-void upload_texture(texture_t *t, const miptex_t *mip, const byte *texture_data, int mip_level)
+unsigned char *convert_8bit_to_24bit(const miptex_t *mip, const byte *texture, int mip_level)
 {
 	size_t i;
-	size_t tex_index;
 	size_t color_index;
 	size_t width = mip->width / (int)pow(2.0, mip_level);
 	size_t height = mip->height / (int)pow(2.0, mip_level);
 	size_t original_size = width * height;
 	size_t expanded_size = (original_size * 3);
-	
 	byte *expanded_data = (byte *)malloc(expanded_size);
-	t->width = width;
-	t->height = height;
 
 	// Convert the image from indexed palette values to RGB values for each pixel.
 	for (i = 0; i < original_size; i++)
 	{
-		color_index = texture_data[i];
+		color_index = texture[i];
 		memcpy(&expanded_data[i*3], &quake_pallete[color_index], 3);
 	}
+
+	return expanded_data;
+}
+
+void upload_texture(texture_t *t, const miptex_t *mip, const byte *texture_data, int mip_level)
+{
+	size_t i;
+	size_t width = mip->width / (int)pow(2.0, mip_level);
+	size_t height = mip->height / (int)pow(2.0, mip_level);
+	size_t original_size = width * height;
+	size_t expanded_size = (original_size * 3);
+	
+	byte *expanded_data = convert_8bit_to_24bit(mip, texture_data, mip_level);
+	t->width = width;
+	t->height = height;
 
 	glBindTexture(GL_TEXTURE_2D, t->texnum);
 
@@ -131,18 +152,8 @@ void upload_texture(texture_t *t, const miptex_t *mip, const byte *texture_data,
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
+	// Upload the image to OpenGL.
 	glTexImage2D(GL_TEXTURE_2D, mip_level, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, expanded_data);
-	
-	/*
-	if (t->texnum == 24)
-	{
-		FILE *f = fopen("texture.raw", "wb");
-		fwrite(expanded_data, 1, expanded_size, f);
-		fclose(f);
-		exit(0);
-	}*/
-
-	printf("Uploaded texture %d miplevel %d\n", t->texnum, mip_level);
 
 	free(expanded_data);
 }
@@ -173,38 +184,35 @@ int read_textures()
 		texinf = &textureinfos[faces[i].texinfo];
 		texnum = texinf->miptex;
 
-		if (texnum == 0)
+		if ((texnum == 0) || textures[texnum].is_loaded)
 			continue;
 
-		if (!textures[texnum].is_loaded)
+		texinf = &textureinfos[texnum];
+
+		t = &textures[texnum];
+		t->texnum = texnum;
+
+		vector_copy(t->s, texinf->vectorS);
+		vector_copy(t->t, texinf->vectorT);
+		t->distS = texinf->distS;
+		t->distT = texinf->distT;
+
+		// Get the pointer to the mipmap.
+		mip = mipmap_ptrs[texinf->miptex];
+		strcpy(t->name, mip->name);
+		t->width = mip->width;
+		t->height = mip->height;
+
+		// Upload the texture data to OpenGL.
+		for (mip_level = 0; mip_level < MIPLEVELS; mip_level++)
 		{
-			texinf = &textureinfos[texnum];
-
-			t = &textures[texnum];
-			t->texnum = texnum;
-
-			vector_copy(t->s, texinf->vectorS);
-			vector_copy(t->t, texinf->vectorT);
-			t->distS = texinf->distS;
-			t->distT = texinf->distT;
-
-			// Get the pointer to the mipmap.
-			mip = mipmap_ptrs[texinf->miptex];
-			strcpy(t->name, mip->name);
-			t->width = mip->width;
-			t->height = mip->height;
-
-			// Upload the texture data to OpenGL.
-			for (mip_level = 0; mip_level < MIPLEVELS; mip_level++)
-			{
-				texture_data = get_texture(texdata, mip, mip_level);
-				upload_texture(t, mip, texture_data, mip_level);
-				free(texture_data);
-			}
-
-			t->is_loaded = 1;
-			texture_count++;
+			texture_data = get_texture(texdata, mip, mip_level);
+			upload_texture(t, mip, texture_data, mip_level);
+			free(texture_data);
 		}
+
+		t->is_loaded = 1;
+		texture_count++;
 	}
 
 	return 1;
